d32f8f993a9784b394f090ed81e84e020cef164d
Marcin Szuster <szust.marcin@gmail.com>
2018-11-16 17:22:10 +0100

    Moved routes to /routes directory

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const routes = require('./routes');
+const routes = require('./routes/routes');
 require('./database/connect')();
 
 const app = express();

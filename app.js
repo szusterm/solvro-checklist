d99b905b20df7ee0cebdd95f791dc432529f641f
Marcin Szuster <szust.marcin@gmail.com>
2018-11-21 18:38:14 +0100

    Turned off strict mode in a json body parser

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({strict: false}));
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
